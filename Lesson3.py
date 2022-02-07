a9a3d8a01bf4f0b950e73d2d70fd311e05808c8c
Kender <98911087+kender111@users.noreply.github.com>
2022-02-08 00:06:55 +0200

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -1,13 +1,12 @@
 #BMI
-height, weight = 1.7, 60 #Програма як така матиме сенс, якщо замість жорстко заданих змінних задати float(input()) 
+height, weight = 1.7, 60 # The program as such will make sense if instead of the set variables to set float (input ())
 print(18.5 < (weight / height ** 2) < 25)
 #Valid_Number
 input_number = '380123456789'
 check = ('380' == input_number[:3] and len(input_number) == 12 and input_number.isdigit()) # замість .isdigit() можна використати .isdecimal()
 print(check)
-#input_number = '380123456789' #якщо не використовувати and
+#input_number = '380123456789' # if don*t use and
 #check1 = '380' == input_number[:3]
 #check2 = len(input_number) == 12
 #check3 = input_number.isdigit()
-#check = bool(check1) == bool(check2) == bool(check3)
-#print(check)
+#print(bool(check1) == bool(check2) == bool(check3))
