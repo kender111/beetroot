c15bd218e9839876fe6abe1580bd879a3e5ba7cf
Kender <98911087+kender111@users.noreply.github.com>
2022-02-07 23:54:57 +0200

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -2,5 +2,5 @@ height, weight = 1.7, 60 #Програма як така матиме сенс, 
 print(18.5 < (weight / height ** 2) < 25)
 #
 input_number = '380123456789'
-check = ('380' == input_number[:3] and len(input_number) == 12 and input_number.isdigit())
+check = ('380' == input_number[:3] and len(input_number) == 12 and input_number.isdigit()) # замість .isdigit() можна використати .isdecimal()
 print(check)
