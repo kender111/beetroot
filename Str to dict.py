d49926facad3617b7b2ce43eb56ce378080c232c
Kender <98911087+kender111@users.noreply.github.com>
2022-02-13 23:49:13 +0200

    String to dictionary
    Make a program that has some sentence (a string) on input and returns a dict containing all unique words as keys and the number of occurrences as values

---
+++ Str to dict.py
@@ -0,0 +1,16 @@
+# Solution 1
+random_string = 'My cats are the most cheeky cats in the world'
+string_to_list = random_string.split() # split string into substrings
+list_to_dict = dict.fromkeys(string_to_list, 0) # Assigning values to keys
+
+for a in string_to_list: #count the number of occurrences and write to the key values
+    list_to_dict[a] += 1
+print(list_to_dict)
+
+# Solution 2
+from collections import Counter
+random_string = 'My cats are the most cheeky cats in the world'
+list_random_string = random_string.lower().split() # Coercing a string to the same case and splitting into substrings
+dict_random_string = Counter(list_random_string) # Count occurrences of keys in a string
+print(dict_random_string)
+
