c6ca5d2479247e279f8f8ed1d3efd116cea88518
Kender <98911087+kender111@users.noreply.github.com>
2022-02-16 01:43:47 +0200

    Update list of tuples.py

--- Dictionaries and the for loop operator/list of tuples.py
+++ Dictionaries and the for loop operator/list of tuples.py
@@ -1,8 +1,2 @@
- # list_of_tuples = []
- # for i in range(1, 10):
- #     j = i ** 2
- #     list_of_tuples.append((i, j))
- # print(list_of_tuples)
-
 list_of_tuples = [(i, j) for i in range(1, 10) for j in (i * i, )]
 print(list_of_tuples)
